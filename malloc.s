8b07cd0f738d6831a63b3df73f12012f11d0b316
ls17 <ls17@inf.ufpr.br>
2018-05-04 15:25:21 -0300

    Ready for test

--- malloc.s
+++ malloc.s
@@ -15,6 +15,7 @@ iniciaAlocador:
     movq        $0, %rdi                # Parametro da chamada (de modo a retornar a altura atual da brk)
     syscall                             # Chamada ao sistema
     movq        %rax, $topoInicialHeap  # Armazena altura da brk em topoInicialHeap
+    movq        $0, (%rax)              # Indica que o "bloco" esta livre
     popq        %rbp                    # Desmonta registro de ativacao atual e restaura ponteiro para o antigo
     ret                                 # Retorna
 ###################################################################################################################
@@ -32,7 +33,7 @@ finalizaAlocador:
     ret                                 # Retorna
 ###################################################################################################################
 ###################################################################################################################
-# int liberaMem(void* bloco) indica que o bloco esta livre.
+# int liberaMem(void* bloco) indica que o bloco esta livre. (int?????????????????????????????????????????????????)
 ###################################################################################################################
 ###################################################################################################################
 liberaMem:
@@ -51,5 +52,65 @@ liberaMem:
 ###################################################################################################################
 ###################################################################################################################
 alocaMem:
+    pushq       %rbp                    # Empilha endereco-base do registro de ativacao antigo
+    movq        %rsp, %rbp              # Atualiza ponteiro para endereco-base do registro de ativacao atual
+    movq        $topoInicialHeap, %rax  # Obtem topoInicialHeap
+    pushq       %rax                    # Aloca variavel local que aponta para a primeira informacao gerencial
+  loop:
+    movq        -8(%rbp), %rax          # Obtem ponteiro para informacao gerencial do bloco atual
+    movq        $topoInicialHeap, %rbx  # Obtem topoInicialHeap
+    cmpq        %rax, %rbx              # Compara topoInicialHeap com ponteiro para informacao gerencial atual
+    jle         done_loop_miss          # Se nao ha blocos liberados utilizaveis, sai do laco com status "miss"
+    movq        -8(%rbp), %rax          # Obtem ponteiro para informacao gerencial do bloco atual
+    movq        (%rax), %rax            # Obtem informacao gerencial do bloco atual
+    movq        $0, %rbx                # Obtem 0 (que indica "livre")
+    cmpq        %rax, %rbx              # Compara 0 com informacao gerencial do bloco atual
+    jne         do_loop_stuff           # Se o bloco nao esta livre, continua no laco
+    movq        -8(%rbp), %rax          # Obtem ponteiro para informacao gerencial do bloco atual
+    movq        8(%rax), %rax           # Obtem tamanho do bloco atual
+    movq        16(%rbp), %rbx          # Obtem parametro num_bytes
+    cmpq        %rax, %rbx              # Compara num_bytes com tamanho do bloco atual
+    jle         done_loop_hit           # Se o bloco atual e grande o suficiente, sai do laco com status "hit"
+  do_loop_stuff:
+    movq        -8(%rbp), %rax          # Obtem ponteiro para informacao gerencial do bloco atual
+    movq        $8(%rax), %rbx          # Obtem tamanho do bloco atual
+    addq        $16, %rax               # Obtem ponteiro para inicio do bloco atual
+    addq        $rbx, %rax              # Obtem ponteiro para a proxima informacao gerencial
+    movq        %rax, -8(%rbp)          # Atualiza variavel com ponteiro para proxima informacao gerencial
+    jmp         loop                    # Continua no laco
+  done_loop_hit:
+    movq        -8(%rbp), %rax          # Obtem ponteiro para informacao gerencial
+    movq        8(%rax), %rax           # Obtem tamanho do bloco
+    movq        16(%rbp), %rbx          # Obtem parametro num_bytes
+    cmpq        %rax, %rbx              # Compara num_bytes com tamanho do bloco
+    je          done                    # Se o bloco tem o tamanho certo, desvia para o fim da funcao
+    movq        -8(%rbp), %rax          # Obtem ponteiro para informacao gerencial
+    movq        16(%rbp), %rbx          # Obtem parametro num_bytes
+    movq        8(%rax), %rdi           # Obtem tamanho do bloco
+    subq        %rbx, %rdi              # Subtrai tamanho a ser alocado
+    subq        $16, %rdi               # Subtrai espaco ocupado pelas informacoes gerenciais
+    addq        %rbx, %rax              # Obtem ponteiro para final do bloco a ser alocado e inicio do bloco livre restante
+    movq        $0, %rax                # Indica que o bloco restante esta livre
+    movq        %rdi, 8(%rax)           # Estabelece tamanho do bloco restante
+    jmp         done                    # Desvia para o final da funcao
+  done_loop_miss:
+    movq        16(%rbp), %rax          # Obtem parametro num_bytes
+    movq        -8(%rbp), %rbx          # Obtem ponteiro para topo atual da heap
+    addq        $16, %rbx               # Obtem ponteiro para inicio do bloco a ser alocado
+    addq        %rax, %rbx              # Obtem ponteiro para final do bloco a ser alocado
+    movq        $12, %rax               # ID do servico brk
+    movq        %rbx, %rdi              # Parametro da chamada (de modo a atualizar a altura da brk)
+    syscall                             # Chamada ao sistema
+  done:
+    movq        -8(%rbp), %rax          # Obtem ponteiro para informacao gerencial alocada
+    movq        $1, (%rax)              # Indica que o bloco alocado esta ocupado
+    addq        $8, %rax                # Obtem ponteiro para tamanho do bloco alocado
+    movq        16(%rbp), %rbx          # Obtem parametro num_bytes
+    movq        %rbx, (%rax)            # Estabelece tamanho do bloco alocado
+    addq        $8, %rax                # Obtem ponteiro para bloco alocado
+    movq        %rax, %rdi              # Estabelece ponteiro para bloco alocado como valor de retorno
+    addq        $8, %rsp                # Desempilha variavel local
+    popq        %rbp                    # Desmonta registro de ativacao atual e restaura ponteiro para o antigo
+    ret                                 # Retorna
 ###################################################################################################################
 ###################################################################################################################
